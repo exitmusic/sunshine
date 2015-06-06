8c3223f092fd9541f637a53e6e9605f4d7d38b97
Kai Chang <kai.chang@morningstar.com>
2015-06-06 07:11:53 -0500

    Add forecast data and frame layout for first screen

--- app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
@@ -1,10 +1,14 @@
 package com.example.android.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,6 +22,17 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] forecastData = {
+                "Mon - Sunny - 83/63",
+                "Tues - Foggy - 70/46",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Rainy - 64/51",
+                "Fri - Foggy - 70/46",
+                "Sat - Sunny - 75/68",
+                "Sun - Snowy - 32/14"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastData));
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
