506d31c95e9d9c2707352c266b2587bbf2dd2b5d
Kai Chang <kai.chang@morningstar.com>
2015-06-20 07:22:57 -0500

    Remove blank lines

--- app/src/main/java/com/example/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/ForecastFragment.java
@@ -63,10 +63,8 @@ public class ForecastFragment extends Fragment {
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
             weatherTask.execute("94043");
-
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
