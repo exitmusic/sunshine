5096af92375d1501db8ce71e196c9c0339f12278
Kai Chang <kai.chang@morningstar.com>
2015-07-17 11:25:49 -0500

    Don't need full path to class

--- app/src/androidTest/java/com/example/android/sunshine/data/TestDb.java
+++ app/src/androidTest/java/com/example/android/sunshine/data/TestDb.java
@@ -23,7 +23,7 @@ public class TestDb extends AndroidTestCase {
 
     // Since we want each test to start with a clean slate
     void deleteTheDatabase() {
-        mContext.deleteDatabase(com.example.android.sunshine.data.WeatherDbHelper.DATABASE_NAME);
+        mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
     }
 
     /*
