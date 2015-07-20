d2500247ed60f8e2b059184f3d00d17af9d3a6c6
Kai Chang <kai.chang@morningstar.com>
2015-07-20 08:43:36 -0500

    Write unit tests for inserting rows into location table

--- app/src/androidTest/java/com/example/android/sunshine/data/TestDb.java
+++ app/src/androidTest/java/com/example/android/sunshine/data/TestDb.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.sunshine.data;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
@@ -112,22 +113,33 @@ public class TestDb extends AndroidTestCase {
     */
     public void testLocationTable() {
         // First step: Get reference to writable database
+        SQLiteDatabase db = new WeatherDbHelper(this.mContext).getWritableDatabase();
 
         // Create ContentValues of what you want to insert
         // (you can use the createNorthPoleLocationValues if you wish)
+        ContentValues testValues = TestUtilities.createNorthPoleLocationValues();
 
         // Insert ContentValues into database and get a row ID back
+        long rowId = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, testValues);
+
+        // Verify a row was returned
+        assertTrue(rowId != -1);
 
         // Query the database and receive a Cursor back
+        Cursor c = db.query(WeatherContract.LocationEntry.TABLE_NAME, null, null, null, null, null, null, null);
 
         // Move the cursor to a valid database row
+        c.moveToFirst();
 
         // Validate data in resulting Cursor with the original ContentValues
         // (you can use the validateCurrentRecord function in TestUtilities to validate the
         // query if you like)
+        String error = "not expected";
+        TestUtilities.validateCurrentRecord(error, c, testValues);
 
         // Finally, close the cursor and database
-
+        c.close();
+        db.close();
     }
 
     /*
