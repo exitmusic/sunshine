049db95d28f97dc9a5b77414441873201e7a8000
Kai Chang <kai.chang@morningstar.com>
2015-08-04 20:08:59 -0500

    Determine layoutId from the data cursor position

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -40,8 +40,15 @@ public class ForecastAdapter extends CursorAdapter {
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         // Choose the layout type
         int viewType = getItemViewType(cursor.getPosition());
-        int layoutId = -1;
-        // TODO: Determine layoutId from viewType
+        int layoutId;
+
+        // Determine layoutId from viewType
+        if (viewType == VIEW_TYPE_TODAY) {
+            layoutId = R.layout.list_item_forecast_today;
+        } else {
+            layoutId = R.layout.list_item_forecast;
+        }
+
         return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
